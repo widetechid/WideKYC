602d5951a25022ed4dbd0332008921183874f75e
WideTechId - Mobile Team <103232763+widetechidmobile@users.noreply.github.com>
2022-11-18 10:34:19 +0700

    Update Initialize.java

--- example/merchant-server/src/main/merchantServer/src/main/java/com/wide/widekyc/merchantserver/Initialize.java
+++ example/merchant-server/src/main/merchantServer/src/main/java/com/wide/widekyc/merchantserver/Initialize.java
@@ -1,4 +1,4 @@
-package com.wide.ekycaggr.merchantserver;
+package com.wide.widekyc.merchantserver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
